8e2a8645d21c11c5aeec091d19b6a1c6f84e7667
fengfeilong <fengfeilongv5@163.com>
2020-11-03 18:38:55 +0800

    Flowable part2

--- app/src/main/java/com/example/rxjavafunny/FlowableHelper.java
+++ app/src/main/java/com/example/rxjavafunny/FlowableHelper.java
@@ -9,10 +9,13 @@ import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.FlowableEmitter;
 import io.reactivex.FlowableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Flowable
  * 参考：https://www.jianshu.com/p/9b1304435564
+ * https://www.jianshu.com/p/a75ecf461e02
  * Flowable里默认有一个大小为128的缓冲区, 当上下游工作在不同的线程中时, 上游就会先把事件发送到这个缓冲区中
  */
 public class FlowableHelper {
@@ -21,6 +24,10 @@ public class FlowableHelper {
 
     public static void testFlowable() {
 
+        /**
+         * BackpressureStrategy.ERROR
+         * 上下游流速不均衡时，抛出异常MissingBackpressureException
+         */
         Flowable.create(new FlowableOnSubscribe<Integer>() {
             @Override
             public void subscribe(FlowableEmitter<Integer> e) throws Exception {
@@ -44,7 +51,121 @@ public class FlowableHelper {
 
                     @Override
                     public void onNext(Integer integer) {
-                        Log.e(TAG, "onNext: " + integer);
+                        Log.e(TAG, "onNext(ERROR): " + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        Log.e(TAG, "onError: " + t.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e(TAG, "onComplete: ");
+                    }
+                });
+
+        /**
+         * BackpressureStrategy.BUFFER
+         * 上游无论发送多少数据都会存在缓冲区中，缓冲区大小无限制
+         * 弊端：如果上下游在不同线程，上游不断发数据下游不request时，存在OOM风险
+         */
+        Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> e) throws Exception {
+                for (int i = 0; i < 1000; i++) {
+                    e.onNext(i);
+                }
+                e.onComplete();
+            }
+        }, BackpressureStrategy.BUFFER)  //设置背压策略为BURRER时，上游无论发送多少数据都会存在缓冲区中，缓冲区大小无限制
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        Log.e(TAG, "onSubscribe: ");
+                        //向上游拉取多少条数据
+                        mSubscription = s;
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.e(TAG, "onNext（BUFFER）: " + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        Log.e(TAG, "onError: " + t.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e(TAG, "onComplete: ");
+                    }
+                });
+
+        /**
+         * BackpressureStrategy.DROP
+         *
+         */
+        Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> e) throws Exception {
+                for (int i = 0; ; i++) {
+                    e.onNext(i);
+                }
+            }
+        }, BackpressureStrategy.DROP)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        Log.e(TAG, "onSubscribe: ");
+                        //向上游拉取多少条数据
+                        mSubscription = s;
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.e(TAG, "onNext（DROP）: " + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+                        Log.e(TAG, "onError: " + t.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e(TAG, "onComplete: ");
+                    }
+                });
+
+        /**
+         * BackpressureStrategy.LATEST
+         *
+         */
+        Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(FlowableEmitter<Integer> e) throws Exception {
+                for (int i = 0; ; i++) {
+                    e.onNext(i);
+                }
+            }
+        }, BackpressureStrategy.LATEST)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Integer>() {
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        Log.e(TAG, "onSubscribe: ");
+                        //向上游拉取多少条数据
+                        mSubscription = s;
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.e(TAG, "onNext（DROP）: " + integer);
                     }
 
                     @Override
